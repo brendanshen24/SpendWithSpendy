b0cc0391a4c173973d17c08382213c5341d94424
jak28 <jak28@sfu.ca>
2025-11-08 18:31:17 -0600

    home screen money test

--- pages/homePage.tsx
+++ pages/homePage.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function HomeScreen() {
+  const money: number = 20.0; // float
+
   return (
-    <View style={styles.container}>
-      <Text>🏠 Home Page</Text>
+    <View className="flex-1 bg-white items-center justify-center">
+      <View className="absolute top-4 left-4">
+        <Text className="text-2xl font-bold">{`$${money.toFixed(2)}`}</Text>
+      </View>
+
+      <Text className="text-lg">🏠 Home Page</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
