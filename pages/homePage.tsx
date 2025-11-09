660f23d1a4aeb157f8a9a8646bb20bc8160d3e21
jak28 <jak28@sfu.ca>
2025-11-08 18:32:18 -0600

    small test change

--- pages/homePage.tsx
+++ pages/homePage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 export default function HomeScreen() {
-  const money: number = 20.0; // float
+  const money: number = 20.0;
 
   return (
     <View className="flex-1 bg-white items-center justify-center">
